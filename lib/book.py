e01288c919dbf09d246ca621d966e0fd9d57bd95
Rem Nikolsky <r3mnik@gmail.com>
2021-09-23 14:16:00 +0300

    Add get_details for Book

--- lib/book.py
+++ lib/book.py
@@ -1,7 +1,29 @@
 from bs4 import BeautifulSoup
+import pdb
+
+from lib.http_request import HttpRequest
 
 class Book:
     def __init__(self, post_header):
         ref_elem = post_header.find('a')
         self.url = ref_elem['href']
-        print(self.url)
+        parsed_uri = self.url.split('/')
+        self.code = parsed_uri[-2]
+        self.category = parsed_uri[-3]
+
+    def get_details(self):
+        request = HttpRequest()
+        req = request.get(self.url)
+
+        bs = BeautifulSoup(req.text, 'html.parser')
+
+        book_details = bs.find('div', {'class':'book-details'})
+        self.name = book_details.find('h1', {'class':'book-title'}).getText()
+
+        attributes_list = book_details.find_all('p')
+        self.author = self.__clear_pulled_string(attributes_list[0].getText())
+        self.published = self.__clear_pulled_string(attributes_list[1].getText())
+        self.publisher = self.__clear_pulled_string(attributes_list[3].getText())
+
+    def __clear_pulled_string(self, str_val):
+        return str_val.strip().replace('\t', '').replace('\n', '')
--- lib/page.py
+++ lib/page.py
@@ -1,4 +1,6 @@
 from bs4 import BeautifulSoup
+import time
+import pdb
 
 from lib.http_request import HttpRequest
 from lib.book import Book
@@ -11,5 +13,18 @@ class Page:
         bs = BeautifulSoup(req.text, 'html.parser')
 
         books_list = bs.find_all('div', {'class':'book-details'})
+        
+        self.books = []
+
         for book_div in books_list:
-            book = Book(book_div)
+            self.books.append(Book(book_div))
+
+        for book in self.books:
+            time.sleep(1)
+            book.get_details()
+
+            print("Code: {}".format(book.code))
+            print("Name: {}".format(book.name))
+            print("Author: {}".format(book.author))
+            print("Published: {}".format(book.published))
+            print("Publisher: {}".format(book.publisher))
